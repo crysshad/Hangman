26cb1762e8393937cd3a59975d8749f9e39cffed
Jing <Jing@10.0.0.142>
2017-06-27 00:17:47 -0400

    Commit627176

--- HangmanGame/src/hangManGame/PickWord.java
+++ HangmanGame/src/hangManGame/PickWord.java
@@ -7,45 +7,56 @@ public class PickWord extends HangmanLogic {
 
 	public String pickCategory() {
 
-		
 		// show user the possible selections
 		System.out.println("Category" + Arrays.toString(super.category));
 
-		/*
-		 * if the user selection matches a category selection then output the
-		 * category otherwise ask user to retry
-		 */
 		
-		boolean match =false;
+		// ask user to select a category
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the category you want to select: ");
+		String userCategorySelection = scan.next();
 		
-		do{
-			
-			// ask user to select a category
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Enter the category you want to select: ");
+		boolean match = false;
+		
+		do {
 
-			String userCategorySelection = scan.next();
-			
-			
+			/*
+			 * if the user selection matches a category selection then output the
+			 * category otherwise ask user to retry
+			 */
 			if (userCategorySelection.equals(super.category[0]) || userCategorySelection.equals(super.category[1])) {
 
 				System.out.println("You have chosen" + " " + userCategorySelection);
+
 				match = true;
 			}
 
 			else {
 
 				System.out.println("Please enter one of the selection shown on the list above");
+				System.out.println("Enter the category you want to select: ");
+				userCategorySelection = scan.next();
 				match = false;
 			}
-			
+
 		}
-		
+
 		while (match == false);
+
 		
+		  if (userCategorySelection.equals("fruit")){
+		  
+			  String fruitList[]=super.fruit;
+			  System.out.println("Category" + Arrays.toString(fruitList));
+		  }
+		  
+		 else{
+			 
+			 String clothingItemList[]=super.clothingItem;
+			 System.out.println("Category" + Arrays.toString(clothingItemList));
+		 }
 		
-		
-	
+
 		return "Fruit";
 	}
 
