b9667a30d8ce3140e00b13216689b0759e8b5c54
Jing <Jing@10.0.0.142>
2017-06-27 01:04:39 -0400

    commit626178

--- HangmanGame/src/hangManGame/PickWord.java
+++ HangmanGame/src/hangManGame/PickWord.java
@@ -6,24 +6,31 @@ import java.util.Scanner;
 
 public class PickWord extends HangmanLogic {
 
+	String wordSelected = "";
+
 	public String pickCategory() {
 
-		// show user the possible selections
+		/******************************************************************************
+		 * show user the possible selections
+		 *******************************************************************************/
+
 		System.out.println("Category" + Arrays.toString(super.category));
 
-		
-		// ask user to select a category
+		/******************************************************************************
+		 * ask user to select a category
+		 *******************************************************************************/
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the category you want to select: ");
 		String userCategorySelection = scan.next();
-		
+
 		boolean match = false;
-		
+
 		do {
 
 			/*
-			 * if the user selection matches a category selection then output the
-			 * category otherwise ask user to retry
+			 * if the user selection matches a category selection then output
+			 * the category otherwise ask user to retry
 			 */
 			if (userCategorySelection.equals(super.category[0]) || userCategorySelection.equals(super.category[1])) {
 
@@ -43,39 +50,54 @@ public class PickWord extends HangmanLogic {
 		}
 
 		while (match == false);
-		
-		 Random random = new Random();
-		 
-		 String fruitWordSelected = "apple";
-		 String clothingWordSelected = "shoe";
-		//retrieve a list of words based on the category selected
-		  if (userCategorySelection.equals("fruit")){
-		  
-			  String fruitList[]=super.fruit;
-			  System.out.println("Category" + Arrays.toString(fruitList));
-			  
-			 
-			  int fruitIndex = random.nextInt(fruitList.length);
-			  System.out.println(fruitList[fruitIndex]);
-			  
-			  fruitWordSelected = fruitList[fruitIndex];
-		  }
-		  
-		 else{
-			 
-			 String clothingItemList[]=super.clothingItem;
-			 System.out.println("Category" + Arrays.toString(clothingItemList));
-			 
-			 int clothingIndex = random.nextInt(clothingItemList.length);
-			  System.out.println(clothingItemList[clothingIndex]);
-			  clothingWordSelected = clothingItemList[clothingIndex];
-		 }
-		
-		  
-		  System.out.println(fruitWordSelected);
-		  System.out.println(clothingWordSelected);
+
+		/******************************************************************************
+		 * Randomly select a word from the category selected by the user Store
+		 * the word in category fruit in fruitWordSelected Store the word in
+		 * category clothingItem in clothingWordSelected
+		 *******************************************************************************/
+
+		Random random = new Random();
+
+		this.wordSelected = wordSelected;
+
+		if (userCategorySelection.equals("fruit")) {
+
+			String fruitList[] = super.fruit;
+			System.out.println("Category" + Arrays.toString(fruitList));
+
+			int fruitIndex = random.nextInt(fruitList.length);
+			System.out.println(fruitList[fruitIndex]);
+
+			wordSelected = fruitList[fruitIndex];
+		}
+
+		else {
+
+			String clothingItemList[] = super.clothingItem;
+			System.out.println("Category" + Arrays.toString(clothingItemList));
+
+			int clothingIndex = random.nextInt(clothingItemList.length);
+			System.out.println(clothingItemList[clothingIndex]);
+			wordSelected = clothingItemList[clothingIndex];
+		}
 
 		return "Fruit";
+	}
+
+	/******************************************************************************
+	 * Getter and Setter
+	 *******************************************************************************/
+
+	public void settWord(String fw) {
+
+		this.wordSelected = fw;
+	}
+
+	public String getWord() {
+
+		return wordSelected;
+
 	}
 
 }
