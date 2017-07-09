0eac58ee4307ad16601c780af73cafcede1b6f5d
Jing <Jing@10.0.0.142>
2017-07-09 19:53:28 -0400

    7_9_2017_Commit13

---
+++ HangmanGame/src/hangManGame/Hangman.java
@@ -0,0 +1,16 @@
+package hangManGame;
+
+public class Hangman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HangmanGameLogic pw = new HangmanGameLogic();
+
+		pw.pickWord();
+
+		pw.prompt();
+
+	}
+
+}
